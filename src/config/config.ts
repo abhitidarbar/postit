e9be51097ba13a93d48fb882ec7e9297d968fc5d
Abhiti Darbar <darbarabhiti@gmail.com>
2024-03-15 00:35:06 +0530

    update rpc url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export default {
-  GNO_JSONRPC_URL: "http://localhost:26657",
+  GNO_JSONRPC_URL: "https://rpc.gno.land:443",
   GNO_POSTIT_REALM: "gno.land/r/demo/postit",
   GAS_FEE: 1,
   GAS_WANTED: 8000000,
