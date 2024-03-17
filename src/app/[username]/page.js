ac22f3ab6d0e49b986b4d2a00c323966f0e67eac
Abhiti Darbar <darbarabhiti@gmail.com>
2024-03-18 02:42:15 +0530

    user profile fixes

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -30,6 +30,7 @@ export default function Profile({ params }) {
   const [load, setLoad] = useState(true);
   const [found, setFound] = useState(true);
   const [offset, setOffset] = useState(0);
+
   const provider = new GnoJSONRPCProvider(config.GNO_JSONRPC_URL);
 
   const handleMouseEnter = () => {
@@ -192,7 +193,7 @@ export default function Profile({ params }) {
               document.getElementById("profile_picture").showModal()
             }
           >
-            {true && (
+            {getFromLocalStorage(defaultAddressKey) === user.Address && (
               <span
                 className={
                   "absolute bg-transparent border-transparent hover:visible " +
@@ -244,7 +245,7 @@ export default function Profile({ params }) {
               alt="Rounded avatar"
             />
           </div>
-          {true && (
+          {getFromLocalStorage(defaultAddressKey) === user.Address && (
             <button
               className="mt-12 ml-auto mr-4 btn btn-sm h-9 w-28 rounded-full bg-black hover:bg-grey-900 text-white border-gray-500"
               onClick={() =>
@@ -331,44 +332,47 @@ export default function Profile({ params }) {
             {errorMessage && (
               <p className="text-red-500 text-xs mt-4">{errorMessage}</p>
             )}
+            {getFromLocalStorage(defaultAddressKey) === user.Address && (
+              <div className="flex flex-col items-center">
+                <input
+                  className="text-xs mt-1 mb-6 w-2/3 ml-2"
+                  type="file"
+                  accept="image/x-png,image/gif,image/jpeg"
+                  onChange={(e) => {
+                    handleFileChange(e);
+                  }}
+                />
 
-            <input
-              className="text-xs mt-1 mb-6 w-1/3 ml-2"
-              type="file"
-              accept="image/x-png,image/gif,image/jpeg"
-              onChange={(e) => {
-                handleFileChange(e);
-              }}
-            />
-
-            <div className="">
-              <button
-                type="button"
-                className={
-                  "flex text-white bg-sky-500 hover:bg-sky-600 disabled:bg-gray-500 font-bold rounded-full text-md py-1.5 text-center mb-2 px-8"
-                }
-                onClick={() => {
-                  updateAvatarTx();
-                }}
-                disabled={loading}
-              >
-                Update Avatar
-                {loading ? (
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="ml-2 animate-spin"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
+                <div className="">
+                  <button
+                    type="button"
+                    className={
+                      "flex text-white bg-sky-500 hover:bg-sky-600 disabled:bg-gray-500 font-bold rounded-full text-md py-1.5 text-center mb-2 px-8"
+                    }
+                    onClick={() => {
+                      updateAvatarTx();
+                    }}
+                    disabled={loading}
                   >
-                    <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
-                  </svg>
-                ) : (
-                  ""
-                )}
-              </button>
-            </div>
+                    Update Avatar
+                    {loading ? (
+                      <svg
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="ml-2 animate-spin"
+                        viewBox="0 0 1792 1792"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
+                      </svg>
+                    ) : (
+                      ""
+                    )}
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         </dialog>
 
